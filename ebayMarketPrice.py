95b28370ea9730953613bf053fbe8531721900a4
Luke Singham <lukesingham@gmail.com>
2022-03-05 19:13:45 +0200

    Adapt script for bot detection

--- ebayMarketPrice.py
+++ ebayMarketPrice.py
@@ -1,37 +1,37 @@
-import requests
-from bs4 import BeautifulSoup
-import numpy as np
+import codecs
 import matplotlib.pyplot as plt
+import numpy as np
+from bs4 import BeautifulSoup
 
-# URL of historical prices of a product
-url = "https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=magic%20trackpad%202&_sacat=3676&LH_Sold=1&LH_Complete=1&Colour=White&_dcat=23160&rt=nc&_udlo=15"
-
-# Get page
-r = requests.get(url) 
+# check webpage.html file exists
+try:
+    # Save file first can no longer use requests lib due to ebay bot detection
+    file = codecs.open("html/webpage.html", "r", "utf-8")
+    html = file.read()
+    file.close()
+except FileNotFoundError:
+    print("File not found - first save webpage.html file")
+    exit()
 
 # Parse the page
-soup = BeautifulSoup(r.content, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')
 
-# Obtain prices TODO write fn for the strip/replace for readability
-Sale_Price = [ (tag.get_text().strip()[1:].replace(',',''))  for tag in soup.find_all("span", class_="POSITIVE") ]
+# Obtain prices
+Sale_Price = [(tag.get_text().strip()[1:].replace(',','')) for tag in soup.find_all("span", class_="POSITIVE")]
 
-# Hack FIX: dates are appearing even though they don't have a class of "POSITIVE"
-# Remove dates
-Sale_Price = [i for i in Sale_Price if "2020" not in i]
-
-# weird string in list
-if 'o' in Sale_Price:
-  Sale_Price.remove('o')
+# Remove strings that share class of POSITIVE
+Sale_Price = [price for price in Sale_Price if all(string not in price for string in ["old", "o"])]
 
 # now change to floats
 Sale_Price = [float(price) for price in Sale_Price]
 
-plt.hist(Sale_Price, bins=20)  # arguments are passed to np.histogram
+plt.hist(Sale_Price, bins=20)
 
 # Average price
 print(np.mean(Sale_Price))
 
 # Average price excluding outlier
-final_prices = [ price for price in Sale_Price if price > 20 ]
+final_prices = [ price for price in Sale_Price if price > 20]
 print(np.mean(final_prices))
 plt.hist(final_prices, bins=50)  # arguments are passed to np.histogram
+plt.show()
