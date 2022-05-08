3731ad6896fd8fb73c53974993c7687af287fa95
Luke Singham <lukesingham@gmail.com>
2022-05-08 15:41:55 +0100

    fmt

--- ebayMarketPrice.py
+++ ebayMarketPrice.py
@@ -14,13 +14,18 @@ except FileNotFoundError:
     exit()
 
 # Parse the page
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, "html.parser")
 
 # Obtain prices
-Sale_Price = [(tag.get_text().strip()[1:].replace(',','')) for tag in soup.find_all("span", class_="POSITIVE")]
+Sale_Price = [
+    (tag.get_text().strip()[1:].replace(",", ""))
+    for tag in soup.find_all("span", class_="POSITIVE")
+]
 
 # Remove strings that share class of POSITIVE
-Sale_Price = [price for price in Sale_Price if all(string not in price for string in ["old", "o"])]
+Sale_Price = [
+    price for price in Sale_Price if all(string not in price for string in ["old", "o"])
+]
 
 # now change to floats
 Sale_Price = [float(price) for price in Sale_Price]
@@ -31,7 +36,7 @@ plt.hist(Sale_Price, bins=20)
 print(np.mean(Sale_Price))
 
 # Average price excluding outlier
-final_prices = [ price for price in Sale_Price if price > 20]
+final_prices = [price for price in Sale_Price if price > 20]
 print(np.mean(final_prices))
 plt.hist(final_prices, bins=50)  # arguments are passed to np.histogram
 plt.show()
