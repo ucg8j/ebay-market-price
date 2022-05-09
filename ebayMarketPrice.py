2d117b8ea4355dc740d036f3bbbc81d3cb90f040
Luke Singham <lukesingham@gmail.com>
2022-05-09 15:24:05 +0100

    Various improvements

--- ebayMarketPrice.py
+++ ebayMarketPrice.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from bs4 import BeautifulSoup
 
+
+LOW_OUTLIERS = 20
+HIGH_OUTLIERS = 250
+
 # check webpage.html file exists
 try:
     # Save file first can no longer use requests lib due to ebay bot detection
@@ -30,13 +34,21 @@ Sale_Price = [
 # now change to floats
 Sale_Price = [float(price) for price in Sale_Price]
 
-plt.hist(Sale_Price, bins=20)
+# Initial plot to see outliers.
+# plt.hist(Sale_Price, bins=20)
 
-# Average price
-print(np.mean(Sale_Price))
+# Average price excluding outliers
+final_prices = [
+    price for price in Sale_Price if price > LOW_OUTLIERS and price < HIGH_OUTLIERS
+]
+pct_25, pct_75 = np.percentile(final_prices, [25, 75])
 
-# Average price excluding outlier
-final_prices = [price for price in Sale_Price if price > 20]
-print(np.mean(final_prices))
-plt.hist(final_prices, bins=50)  # arguments are passed to np.histogram
+print(
+    f"mean: {np.mean(final_prices)}\n"
+    f"median: {np.median(final_prices)}\n"
+    f"median: {np.median(final_prices)}\n"
+    f"25th percentile: {pct_25}\n"
+    f"75th percentile: {pct_75}\n"
+)
+plt.hist(final_prices, bins=20)
 plt.show()
