26b3cb2c785f82c4d9dc40ee3ea2dc8222b68485
Luke Singham <lukesingham@gmail.com>
2022-05-27 11:38:25 +0100

    Various improvements

--- ebayMarketPrice.py
+++ ebayMarketPrice.py
@@ -1,16 +1,17 @@
 import codecs
+
 import matplotlib.pyplot as plt
 import numpy as np
 from bs4 import BeautifulSoup
 
+LOW_OUTLIERS = 551
+HIGH_OUTLIERS = 2500
+ITEM = "m1-16gb-pro"
+FIRST_N_PRICES = 50
 
-LOW_OUTLIERS = 20
-HIGH_OUTLIERS = 250
-
-# check webpage.html file exists
 try:
-    # Save file first can no longer use requests lib due to ebay bot detection
-    file = codecs.open("html/webpage.html", "r", "utf-8")
+    # Save file as f"{ITEM}.html" first can no longer use requests lib due to ebay bot detection
+    file = codecs.open(f"html/{ITEM}.html", "r", "utf-8")
     html = file.read()
     file.close()
 except FileNotFoundError:
@@ -21,34 +22,41 @@ except FileNotFoundError:
 soup = BeautifulSoup(html, "html.parser")
 
 # Obtain prices
-Sale_Price = [
+sale_price = [
     (tag.get_text().strip()[1:].replace(",", ""))
     for tag in soup.find_all("span", class_="POSITIVE")
 ]
 
 # Remove strings that share class of POSITIVE
-Sale_Price = [
-    price for price in Sale_Price if all(string not in price for string in ["old", "o"])
+sale_price = [
+    price for price in sale_price if all(string not in price for string in ["old", "o"])
 ]
 
 # now change to floats
-Sale_Price = [float(price) for price in Sale_Price]
+sale_price = [float(price) for price in sale_price]
+
+# limit to first x prices
+sale_price = sale_price[:FIRST_N_PRICES]
 
 # Initial plot to see outliers.
 # plt.hist(Sale_Price, bins=20)
 
 # Average price excluding outliers
 final_prices = [
-    price for price in Sale_Price if price > LOW_OUTLIERS and price < HIGH_OUTLIERS
+    price for price in sale_price if price > LOW_OUTLIERS and price < HIGH_OUTLIERS
 ]
 pct_25, pct_75 = np.percentile(final_prices, [25, 75])
 
-print(
-    f"mean: {np.mean(final_prices)}\n"
-    f"median: {np.median(final_prices)}\n"
-    f"median: {np.median(final_prices)}\n"
-    f"25th percentile: {pct_25}\n"
-    f"75th percentile: {pct_75}\n"
+stats = (
+    f"mean: {round(np.mean(final_prices), 2)}\n"
+    f"median: {round(np.median(final_prices), 2)}\n"
+    f"25th %ile: {round(pct_25, 2)}\n"
+    f"75th %ile: {round(pct_75, 2)}\n"
 )
+
+print(stats)
+
 plt.hist(final_prices, bins=20)
+plt.title(f"{ITEM}")
+plt.figtext(0.67, 0.7, stats)
 plt.show()
